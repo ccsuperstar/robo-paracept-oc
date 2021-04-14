54a58630a3e291b80aefcb1644e18c0bb87c2dc1
Caroline BUZENET <caroline.buzenet@openclassrooms.com>
2021-04-14 09:44:34 +0200

    Update MergeReports.php

--- src/MergeReports.php
+++ src/MergeReports.php
@@ -398,13 +398,13 @@ class MergeHTMLReportsTask extends BaseTask implements TaskInterface, MergeRepor
      * @param $dstFile \DOMDocument - destination file
      */
     private function updateButtons($dstFile){
-        $nodes = (new \DOMXPath($dstFile))->query("//div[@class='layout']/table/tr[contains(@class, 'scenarioRow')]");
+        $nodes = (new \DOMXPath($dstFile))->query("//table/tr[contains(@class,'scenarioRow')]");
         for($i=2;$i<$nodes->length;$i+=2){
             $n = $i/2 + 1;
             $p = $nodes->item($i)->childNodes->item(1)->childNodes->item(1);
             $table = $nodes->item($i+1)->childNodes->item(1)->childNodes->item(1);
-            // $p->setAttribute('onclick',"showHide('$n', this)");
-            // $table->setAttribute('id',"stepContainer".$n);
+            $p->setAttribute('onclick',"showHide('$n', this)");
+            $table->setAttribute('id',"stepContainer".$n);
         }
     }
 
